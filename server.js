f94483df70c16aaee2509e4616202d40655b240d
Rudraz <Rudraz@users.noreply.github.com>
2023-02-28 16:46:11 +0800

    new changes

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
+    // Setup empty JS object to act as endpoint for all routes
+const projectData = {};
+
+
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
 
 // Initialize the main project folder
 app.use(express.static('website'));
+app.use(cors());
+
 
 
 // Setup Server
